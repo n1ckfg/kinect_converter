0ab27133827520f2200650ed25679ce115f14310
Nick Fox-Gieg <nick@fox-gieg.com>
2023-04-16 21:36:10 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+'''
 from kinect_converter import get_intrinsics
 from kinect_converter import extract_frames 
 from kinect_converter import do_inpainting
@@ -8,3 +9,4 @@ from kinect_converter import save_point_cloud
 from kinect_converter import save_mesh
 from kinect_converter import cv2_to_o3d
 from kinect_converter import uvd_to_xyz
+'''
